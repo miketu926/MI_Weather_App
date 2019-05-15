72319f603be2c7663c7885d37831c20b63e77fe3
Mike <miketu926@gmail.com>
2019-05-15 18:34:58 -0400

    Added files

---
+++ script.js
@@ -0,0 +1,294 @@
+const locationApi = {
+  "country": "US",
+  "city": "Jersey City",
+  "region": "New Jersey",
+  "regionCode": "NJ",
+  "county": "Hudson",
+  "countyCode": "017",
+  "latitude": "40.7221",
+  "longitude": "-74.0469",
+  "accuracy": 4
+};
+
+const forecastApi = {
+  "daily": {
+    "summary": "No precipitation throughout the week, with high temperatures rising to 66°F",
+    "icon": "sunny",
+    "data": [
+      {
+        "time": null,
+        "summary": "Mostly cloudy throughout the day.",
+        "icon": "cloudy",
+        "sunriseTime": null,
+        "sunsetTime": null,
+        "moonPhase": 0.06,
+        "precipIntensity": 0.0004,
+        "precipIntensityMax": 0.0015,
+        "precipIntensityMaxTime": null,
+        "precipProbability": 0.04,
+        "precipType": "rain",
+        "temperatureHigh": 59.02,
+        "temperatureHighTime": null,
+        "temperatureLow": 54.93,
+        "temperatureLowTime": null,
+        "apparentTemperatureHigh": 59.02,
+        "apparentTemperatureHighTime": null,
+        "apparentTemperatureLow": 54.93,
+        "apparentTemperatureLowTime": null,
+        "dewPoint": 49.49,
+        "humidity": 0.82,
+        "pressure": 1012.48,
+        "windSpeed": 8.35,
+        "windGust": 15.48,
+        "windGustTime": null,
+        "windBearing": 239,
+        "cloudCover": 0.81,
+        "uvIndex": 5,
+        "uvIndexTime": null,
+        "visibility": 9.74,
+        "ozone": 383.05,
+        "temperatureMin": 52.04,
+        "temperatureMinTime": null,
+        "temperatureMax": 59.02,
+        "temperatureMaxTime": null,
+        "apparentTemperatureMin": 52.04,
+        "apparentTemperatureMinTime": null,
+        "apparentTemperatureMax": 59.02,
+        "apparentTemperatureMaxTime": null
+      },
+      {
+        "time": null,
+        "summary": "Mostly sunny throughout the day.",
+        "icon": "sunny",
+        "sunriseTime": null,
+        "sunsetTime": null,
+        "moonPhase": 0.06,
+        "precipIntensity": 0.0004,
+        "precipIntensityMax": 0.0015,
+        "precipIntensityMaxTime": null,
+        "precipProbability": 0.04,
+        "precipType": "rain",
+        "temperatureHigh": 59.02,
+        "temperatureHighTime": null,
+        "temperatureLow": 54.93,
+        "temperatureLowTime": null,
+        "apparentTemperatureHigh": 59.02,
+        "apparentTemperatureHighTime": null,
+        "apparentTemperatureLow": 54.93,
+        "apparentTemperatureLowTime": null,
+        "dewPoint": 49.49,
+        "humidity": 0.82,
+        "pressure": 1012.48,
+        "windSpeed": 8.35,
+        "windGust": 15.48,
+        "windGustTime": null,
+        "windBearing": 239,
+        "cloudCover": 0.81,
+        "uvIndex": 5,
+        "uvIndexTime": null,
+        "visibility": 9.74,
+        "ozone": 383.05,
+        "temperatureMin": 52.04,
+        "temperatureMinTime": null,
+        "temperatureMax": 59.02,
+        "temperatureMaxTime": null,
+        "apparentTemperatureMin": 52.04,
+        "apparentTemperatureMinTime": null,
+        "apparentTemperatureMax": 59.02,
+        "apparentTemperatureMaxTime": null
+      },
+      {
+        "time": null,
+        "summary": "Mostly rain throughout the day.",
+        "icon": "rain",
+        "sunriseTime": null,
+        "sunsetTime": null,
+        "moonPhase": 0.06,
+        "precipIntensity": 0.0004,
+        "precipIntensityMax": 0.0015,
+        "precipIntensityMaxTime": null,
+        "precipProbability": 0.04,
+        "precipType": "rain",
+        "temperatureHigh": 59.02,
+        "temperatureHighTime": null,
+        "temperatureLow": 54.93,
+        "temperatureLowTime": null,
+        "apparentTemperatureHigh": 59.02,
+        "apparentTemperatureHighTime": null,
+        "apparentTemperatureLow": 54.93,
+        "apparentTemperatureLowTime": null,
+        "dewPoint": 49.49,
+        "humidity": 0.82,
+        "pressure": 1012.48,
+        "windSpeed": 8.35,
+        "windGust": 15.48,
+        "windGustTime": null,
+        "windBearing": 239,
+        "cloudCover": 0.81,
+        "uvIndex": 5,
+        "uvIndexTime": null,
+        "visibility": 9.74,
+        "ozone": 383.05,
+        "temperatureMin": 52.04,
+        "temperatureMinTime": null,
+        "temperatureMax": 59.02,
+        "temperatureMaxTime": null,
+        "apparentTemperatureMin": 52.04,
+        "apparentTemperatureMinTime": null,
+        "apparentTemperatureMax": 59.02,
+        "apparentTemperatureMaxTime": null
+      },
+      {
+        "time": null,
+        "summary": "Mostly snow throughout the day.",
+        "icon": "snow",
+        "sunriseTime": null,
+        "sunsetTime": null,
+        "moonPhase": 0.06,
+        "precipIntensity": 0.0004,
+        "precipIntensityMax": 0.0015,
+        "precipIntensityMaxTime": null,
+        "precipProbability": 0.04,
+        "precipType": "rain",
+        "temperatureHigh": 59.02,
+        "temperatureHighTime": null,
+        "temperatureLow": 54.93,
+        "temperatureLowTime": null,
+        "apparentTemperatureHigh": 59.02,
+        "apparentTemperatureHighTime": null,
+        "apparentTemperatureLow": 54.93,
+        "apparentTemperatureLowTime": null,
+        "dewPoint": 49.49,
+        "humidity": 0.82,
+        "pressure": 1012.48,
+        "windSpeed": 8.35,
+        "windGust": 15.48,
+        "windGustTime": null,
+        "windBearing": 239,
+        "cloudCover": 0.81,
+        "uvIndex": 5,
+        "uvIndexTime": null,
+        "visibility": 9.74,
+        "ozone": 383.05,
+        "temperatureMin": 52.04,
+        "temperatureMinTime": null,
+        "temperatureMax": 59.02,
+        "temperatureMaxTime": null,
+        "apparentTemperatureMin": 52.04,
+        "apparentTemperatureMinTime": null,
+        "apparentTemperatureMax": 59.02,
+        "apparentTemperatureMaxTime": null
+      },
+      {
+        "time": null,
+        "summary": "Mostly sunny throughout the day.",
+        "icon": "sunny",
+        "sunriseTime": null,
+        "sunsetTime": null,
+        "moonPhase": 0.06,
+        "precipIntensity": 0.0004,
+        "precipIntensityMax": 0.0015,
+        "precipIntensityMaxTime": null,
+        "precipProbability": 0.04,
+        "precipType": "rain",
+        "temperatureHigh": 59.02,
+        "temperatureHighTime": null,
+        "temperatureLow": 54.93,
+        "temperatureLowTime": null,
+        "apparentTemperatureHigh": 59.02,
+        "apparentTemperatureHighTime": null,
+        "apparentTemperatureLow": 54.93,
+        "apparentTemperatureLowTime": null,
+        "dewPoint": 49.49,
+        "humidity": 0.82,
+        "pressure": 1012.48,
+        "windSpeed": 8.35,
+        "windGust": 15.48,
+        "windGustTime": null,
+        "windBearing": 239,
+        "cloudCover": 0.81,
+        "uvIndex": 5,
+        "uvIndexTime": null,
+        "visibility": 9.74,
+        "ozone": 383.05,
+        "temperatureMin": 52.04,
+        "temperatureMinTime": null,
+        "temperatureMax": 59.02,
+        "temperatureMaxTime": null,
+        "apparentTemperatureMin": 52.04,
+        "apparentTemperatureMinTime": null,
+        "apparentTemperatureMax": 59.02,
+        "apparentTemperatureMaxTime": null
+      }
+    ]
+  }
+};
+
+// extract query string from URL, zip_code & date
+const urlParams = new URLSearchParams(window.location.search);
+const zipCode = urlParams.get('zip_code');
+const date = urlParams.get('date');
+
+// fetch location and forecast with zipCode and date
+const fetchLocation = async (zip) => {
+  const res = await fetch("https://se-weather-api.herokuapp.com/api/v1/geo?zip_code=" + zip, {
+    mode: 'no-cors'
+  });
+  const data = await res.json();
+
+  return data;
+  // data returns a type opaque response (?)
+};
+
+const fetchForecast = async (date, zip) => {
+  const res = await fetch("https://se-weather-api.herokuapp.com/api/v1/forecast?date=" + date + "&zip_code=" + zip, {
+    mode: 'no-cors'
+  });
+  const data = await res.json();
+
+  return data;
+  // data returns a type opaque response (?)
+};
+
+const cap = (string) => {
+  return string[0].toUpperCase() + string.slice(1);
+}
+
+const city = locationApi.city.toUpperCase();
+const state = locationApi.regionCode.toUpperCase();
+
+// dynamic day names
+const dateObj = new Date(date);
+
+const weekday = ["Sunday:", "Monday:", "Tuesday:", "Wednesday:", "Thursday:", "Friday:", "Saturday:"]
+const day2Name = weekday[(dateObj.getDay() + 1) % 7];
+const day3Name = weekday[(dateObj.getDay() + 2) % 7];
+
+// extract temp lows/highs/status
+const day1Low = Math.round(forecastApi.daily.data[0].temperatureLow);
+const day1High = Math.round(forecastApi.daily.data[0].temperatureHigh);
+const day1Status = cap(forecastApi.daily.data[0].icon);
+
+const day2Low = Math.round(forecastApi.daily.data[1].temperatureLow);
+const day2High = Math.round(forecastApi.daily.data[1].temperatureHigh);
+const day2Status = cap(forecastApi.daily.data[1].icon);
+
+const day3Low = Math.round(forecastApi.daily.data[2].temperatureLow);
+const day3High = Math.round(forecastApi.daily.data[2].temperatureHigh);
+const day3Status = cap(forecastApi.daily.data[2].icon);
+
+// fill in day names
+document.getElementById('location').innerHTML = "WEATHER FORECAST FOR " + city + ", " + state;
+document.getElementById('day2-name').innerHTML = day2Name;
+document.getElementById('day3-name').innerHTML = day3Name;
+
+// fill in weather information
+document.getElementById('day1-status').innerHTML = day1Status;
+document.getElementById('day1-temps').innerHTML = day1Low + " / " + day1High + " F"
+document.getElementById('day2-status').innerHTML = day2Status;
+document.getElementById('day2-temps').innerHTML = day2Low + " / " + day2High + " F"
+document.getElementById('day3-status').innerHTML = day3Status;
+document.getElementById('day3-temps').innerHTML = day3Low + " / " + day3High + " F"
+
+
+// file:///Users/mike/Downloads/Job_Search/MovableInk/index.html?zip_code=10011&date=05/15/2019
